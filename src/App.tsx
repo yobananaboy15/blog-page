65608e6b8129682045eda7d61510b8c6dcbb27bc
Axel Thiel <axth2220@gmail.com>
2022-11-22 21:38:03 +0100

    Create blog post type

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+export type BlogPost = {
+  id: string;
+  title: string;
+  leadParagraph: string;
+  text: string;
+  author: string;
+  authorEmail: string;
+  date: Date;
+};
+
 function App() {
   return <div></div>;
 }
