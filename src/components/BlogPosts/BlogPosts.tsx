a9923468b94c26d2c3764f94ba5b771b953237bc
Axel Thiel <axth2220@gmail.com>
2022-11-23 17:44:54 +0100

    Update logic of blog posts component

--- src/components/BlogPosts/BlogPosts.tsx
+++ src/components/BlogPosts/BlogPosts.tsx
@@ -3,30 +3,103 @@ import {
   Card,
   CardActions,
   CardContent,
+  Grid,
   Typography,
 } from "@mui/material";
 
-import { BlogPost } from "../../App";
+import { BlogPost } from "App";
+import { BlogPostModal } from "components/BlogPostModal";
+import { useState } from "react";
 
 type BlogPostProps = {
+  setSelectedBlogPost: (blogPosts: BlogPost) => void;
   blogPosts: BlogPost[];
+  setBlogPosts: (blogPost: BlogPost[]) => void;
 };
 
-export const BlogPosts = ({ blogPosts }: BlogPostProps) => {
+export const BlogPosts = ({
+  setSelectedBlogPost,
+  blogPosts,
+  setBlogPosts,
+}: BlogPostProps) => {
+  const [blogPostToEdit, setBlogPostToEdit] = useState<BlogPost>();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const addBlogPost = (blogPost: BlogPost) => {
+    setBlogPosts([blogPost, ...blogPosts]);
+  };
+
+  const editBlogPost = (updatedBlogPost: BlogPost) => {
+    const newBlogPosts = blogPosts.map((blogPost) =>
+      blogPost.id === updatedBlogPost.id ? updatedBlogPost : blogPost
+    );
+    setBlogPosts(newBlogPosts);
+  };
+
+  const deleteBlogPost = (removeBlogPostId: string) => {
+    setBlogPosts(
+      blogPosts.filter((blogPost) => blogPost.id !== removeBlogPostId)
+    );
+  };
+
   return (
     <div>
-      {blogPosts.map(({ title, leadParagraph, date }) => (
-        <Card>
-          <CardContent>
-            <Typography>{title}</Typography>
-          </CardContent>
-          <Typography>{leadParagraph}</Typography>
-          <Typography>{date.toLocaleDateString()}</Typography>
-          <CardActions>
-            <Button size="small">Läs blogginlägg</Button>
-          </CardActions>
-        </Card>
-      ))}
+      <Button
+        variant="contained"
+        onClick={() => setModalIsOpen(true)}
+        sx={{ mb: 2, mt: 2 }}
+      >
+        Skapa blogginlägg
+      </Button>
+      <Grid container spacing={2}>
+        {blogPosts.map((blogPost) => (
+          <Grid key={blogPost.id} item xs={12} sm={6} md={4}>
+            <Card>
+              <CardContent>
+                <Typography>{blogPost.title}</Typography>
+                <Typography>{blogPost.leadParagraph}</Typography>
+                <Typography>
+                  {blogPost.date.toLocaleDateString("sv-SE")}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="small"
+                  onClick={() => setSelectedBlogPost(blogPost)}
+                >
+                  Läs
+                </Button>
+                <Button
+                  size="small"
+                  onClick={() => {
+                    setBlogPostToEdit(blogPost);
+                    setModalIsOpen(true);
+                  }}
+                >
+                  Redigera
+                </Button>
+                <Button
+                  size="small"
+                  onClick={() => deleteBlogPost(blogPost.id)}
+                >
+                  Radera
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+      {modalIsOpen && (
+        <BlogPostModal
+          isOpen={modalIsOpen}
+          onClose={() => {
+            setModalIsOpen(false);
+            setBlogPostToEdit(undefined);
+          }}
+          blogPostToEdit={blogPostToEdit}
+          submitHandler={blogPostToEdit ? editBlogPost : addBlogPost}
+        />
+      )}
     </div>
   );
 };
