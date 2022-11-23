41dfad8980287a612cccb5d831c94390423557d1
Axel Thiel <axth2220@gmail.com>
2022-11-23 21:04:38 +0100

    Update field validation

--- src/components/BlogPostModal/BlogPostModal.tsx
+++ src/components/BlogPostModal/BlogPostModal.tsx
@@ -7,9 +7,8 @@ import {
   TextField,
 } from "@mui/material";
 import { BlogPost } from "App";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { validateEmail } from "utils/validateEmail";
 
 type BlogPostModalProps = {
   isOpen: boolean;
@@ -41,10 +40,9 @@ export const BlogPostModal = ({
     setFormState({ ...formState, [key]: value });
   };
 
-  const onSubmit = () => {
-    if (!validateEmail(formState.authorEmail)) {
-      return;
-    }
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
     if (blogPostToEdit) {
       submitHandler({ ...formState, id: blogPostToEdit.id, date: new Date() });
     } else {
@@ -64,71 +62,79 @@ export const BlogPostModal = ({
 
   return (
     <Dialog open={isOpen} onClose={onClose}>
-      <DialogTitle>{`${
-        blogPostToEdit ? "Redigera" : "Skapa"
-      } blogginlägg`}</DialogTitle>
-      <DialogContent>
-        <TextField
-          value={formState.title}
-          onChange={(e) => handleChange(e.target.value, "title")}
-          margin="dense"
-          fullWidth
-          name="title"
-          label="Rubrik"
-          type="text"
-          inputProps={{ maxLength: 50 }}
-        />
-        <TextField
-          value={formState.leadParagraph}
-          onChange={(e) => handleChange(e.target.value, "leadParagraph")}
-          margin="dense"
-          fullWidth
-          name="leadParagraph"
-          label="Ingress"
-          type="text"
-          inputProps={{ maxLength: 250 }}
-          multiline
-          rows={2}
-        />
-        <TextField
-          value={formState.text}
-          onChange={(e) => handleChange(e.target.value, "text")}
-          margin="dense"
-          fullWidth
-          name="text"
-          label="Innehåll"
-          type="text"
-          multiline
-          rows={3}
-        />
-        <TextField
-          value={formState.author}
-          onChange={(e) => handleChange(e.target.value, "author")}
-          margin="dense"
-          fullWidth
-          name="author"
-          label="Författare"
-          type="text"
-          inputProps={{ maxLength: 40 }}
-        />
-        <TextField
-          value={formState.authorEmail}
-          onChange={(e) => handleChange(e.target.value, "authorEmail")}
-          margin="dense"
-          fullWidth
-          name="email"
-          label="E-post"
-          type="email"
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button variant="contained" color="error" onClick={onClose}>
-          Avbryt
-        </Button>
-        <Button variant="contained" color="primary" onClick={onSubmit}>
-          {blogPostToEdit ? "Uppdatera" : "Skapa"}
-        </Button>
-      </DialogActions>
+      <form onSubmit={onSubmit}>
+        <DialogTitle>{`${
+          blogPostToEdit ? "Redigera" : "Skapa"
+        } blogginlägg`}</DialogTitle>
+        <DialogContent>
+          <TextField
+            required
+            value={formState.title}
+            onChange={(e) => handleChange(e.target.value, "title")}
+            margin="dense"
+            fullWidth
+            name="title"
+            label="Rubrik"
+            type="text"
+            inputProps={{ maxLength: 50 }}
+          />
+          <TextField
+            required
+            value={formState.leadParagraph}
+            onChange={(e) => handleChange(e.target.value, "leadParagraph")}
+            margin="dense"
+            fullWidth
+            name="leadParagraph"
+            label="Ingress"
+            type="text"
+            inputProps={{ maxLength: 250 }}
+            multiline
+            rows={2}
+          />
+          <TextField
+            required
+            value={formState.text}
+            onChange={(e) => handleChange(e.target.value, "text")}
+            margin="dense"
+            fullWidth
+            name="text"
+            label="Innehåll"
+            type="text"
+            multiline
+            rows={3}
+          />
+          <TextField
+            required
+            value={formState.author}
+            onChange={(e) => handleChange(e.target.value, "author")}
+            margin="dense"
+            fullWidth
+            name="author"
+            label="Författare"
+            type="text"
+            inputProps={{ maxLength: 40 }}
+          />
+          <TextField
+            required
+            value={formState.authorEmail}
+            onChange={(e) => handleChange(e.target.value, "authorEmail")}
+            margin="dense"
+            fullWidth
+            name="email"
+            label="E-post"
+            type="email"
+            inputProps={{ pattern: "\\S+@\\S+\\.\\S+" }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color="error" onClick={onClose}>
+            Avbryt
+          </Button>
+          <Button type="submit" variant="contained" color="primary">
+            {blogPostToEdit ? "Uppdatera" : "Skapa"}
+          </Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 };
--- src/utils/validateEmail.ts
+++
@@ -1,4 +0,0 @@
-export const validateEmail = (email: string) => {
-  var regex = /\S+@\S+\.\S+/;
-  return regex.test(email);
-};
