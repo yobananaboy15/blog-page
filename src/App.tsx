b5840826284c5658edb103f2c76b64d1eb70877d
Axel Thiel <axth2220@gmail.com>
2022-11-23 18:27:03 +0100

    Add BlogPost component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Container, Typography } from "@mui/material";
+import { BlogPost } from "components/BlogPost";
 import { BlogPosts } from "components/BlogPosts";
 import { useState } from "react";
 
@@ -18,11 +19,14 @@ function App() {
 
   return (
     <Container>
-      <Typography variant="h2" sx={{ textAlign: "center" }}>
+      <Typography variant="h2" sx={{ textAlign: "center", mb: 2 }}>
         Bloggsida
       </Typography>
       {selectedBlogPost ? (
-        <div>Blogginlägg</div>
+        <BlogPost
+          goBackCallback={() => setSelectedBlogPost(undefined)}
+          blogPost={selectedBlogPost}
+        />
       ) : (
         <BlogPosts
           setSelectedBlogPost={setSelectedBlogPost}
